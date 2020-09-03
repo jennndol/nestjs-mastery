e139b940cae71c12fdebb51aa62732fc00f549f1
Luthfi <jkt.luthfi@gmail.com>
2020-09-03 13:17:49 +0700

    ADD: basic routing and methods

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,9 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body, HttpCode, HttpStatus } from '@nestjs/common';
+
+
 
 @Controller('coffees')
 export class CoffeesController {
     @Get()
-    findAll(){
+    findAll():string{
         return 'This action returns all coffees';
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string):string{
+        return `This action returns #${id} coffee`;
+    }
+
+    @Post()
+    create(@Body() body) {
+        return body
     }
 }
